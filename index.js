9475067270a281da07bef05a748e7039752c6fe6
mintsweet <0x1304570@gmail.com>
2023-08-03 14:15:00 +1200

    feat: rules based on react-app and prettier

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'ignore' },
+        'newlines-between': 'always-and-inside-groups',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [{ pattern: '~/**', group: 'internal' }],
+      },
+    ],
+  },
+};
